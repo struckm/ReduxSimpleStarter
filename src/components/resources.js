332caf82834b89263e2cdf923c7fdea2a126163c
Mark Struck <markstruck@comcast.net>
2016-08-08 19:06:02 -0500

    Added a new route.

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            Test Resources
+            <ul>
+                <li>Resource One</li>
+                <li>Resource Two</li>
+                <li>Resource Three</li>
+            </ul>
+        </div>
+    );
+};
